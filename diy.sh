ae8e1fce06693c85967a77b646ce2d2fbc069f1a
hhai93 <70931114+hhai93@users.noreply.github.com>
2025-04-17 13:14:22 +0700

    Create diy.sh

---
+++ diy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Custom DIY script for OpenWrt build
+# Add your customizations here
+
+# Example: Modify default LAN IP
+# echo "Customizing LAN IP"
+# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate

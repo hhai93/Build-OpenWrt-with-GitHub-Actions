6eb2d981072cc85e162a1e5a94f0f745a166f8a4
hhai93 <70931114+hhai93@users.noreply.github.com>
2025-04-17 13:53:58 +0700

    Update and rename diy.sh to diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# OpenWrt DIY script part 1 (Before Update feeds)
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default

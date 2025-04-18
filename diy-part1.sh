6826a62047ae2a011619bfe3024807fe7f980115
hhai93 <70931114+hhai93@users.noreply.github.com>
2025-04-18 09:45:57 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,3 @@
-!/bin/bash
-Add a feed source
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#!/bin/bash
+#Add a feed source
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default ##Example feeds
